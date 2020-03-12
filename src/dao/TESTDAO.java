4ef27393e09595e4e3a96a1279682922af5434cc
MrLeaper <750303040@qq.com>
2020-03-12 22:01:32 +0800

    "test"

---
+++ src/dao/TESTDAO.java
@@ -0,0 +1,13 @@
+package dao;
+
+/**
+ * @author leaper
+ * @description 这是TESTDAO类
+ * @date 2020年 03月12日 21:56:04
+ */
+public class TESTDAO {
+    private void test(){
+        System.out.println("test");
+    }
+
+}
